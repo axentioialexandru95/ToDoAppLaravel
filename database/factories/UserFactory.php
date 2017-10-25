97fc06c029b18a5c436023f97b3ad965277dc9d7
Axentioi Alexandru <axentioialexandru95@gmail.com>
2017-10-25 16:14:50 +0300

    Added seeder with faker

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,17 +2,6 @@
 
 use Faker\Generator as Faker;
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| This directory should contain each of the model factory definitions for
-| your application. Factories provide a convenient way to generate new
-| model instances for testing / seeding your application's database.
-|
-*/
-
 $factory->define(App\User::class, function (Faker $faker) {
     static $password;
 
@@ -22,4 +11,10 @@ $factory->define(App\User::class, function (Faker $faker) {
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
+});
+
+$factory->define(App\Todo::class, function(Faker $faker){
+  return [
+    'todo' => $faker->sentence(10)
+  ];
 });
